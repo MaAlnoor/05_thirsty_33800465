71d82444292030e183db965fdb6107b31babca6f
Mohammed Alnoor <mohammed1169@hotmail.co.uk>
2025-11-06 23:28:52 +0000

    first commit

---
+++ routes/main.js
@@ -0,0 +1,38 @@
+// Define our data
+var shopData = {shopName: "Smooth Smoothies", 
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"]
+};
+
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData)
+}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+}); 
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs", shopData)
+});
+
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send("You searched for " + req.query.search_text + " in " + req.query.category);
+});
+
+router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+ 
+router.post("/registered", (req,res) => { 
+    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered! We will send a confirmation email to '+ req.body.email);
+}); 
+
+
+// Export the router object so index.js can access it
+module.exports = router;
